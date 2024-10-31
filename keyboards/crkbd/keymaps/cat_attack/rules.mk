2a22ba0b67ed4eaafa2f6c44795be867b4511682
Armando <armandolv@gmail.com>
2024-10-31 10:20:42 -0700

    Add custom keymap configuration for Plank and Corne

---
+++ keyboards/crkbd/keymaps/cat_attack/rules.mk
@@ -0,0 +1,15 @@
+# Bootloader for the elite-c
+BOOTLOADER = atmel-dfu
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+MUSIC_ENABLE = no
+
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+TAP_DANCE_ENABLE = yes
